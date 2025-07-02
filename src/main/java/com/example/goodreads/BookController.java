4bc64c1469b98ac8ccaa3744145ec2099c5eb7e4
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-07-02 08:06:45 +0000

    Goodreads.com Backend structure

---
+++ src/main/java/com/example/goodreads/BookController.java
@@ -0,0 +1,8 @@
+package com.example.goodreads;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Getmapping;
+
+@RestController
+class Bookcontroller{
+     @Getmapping("/books")
+}
